0dce546b8ced6edd15886bf2c57963f27063237a
Jeff Chien <jeff85@gmail.com>
2014-11-16 01:15:22 -0600

    better error reporting for failed parses

--- src/main/java/org/jchien/twitchbrowser/util/AnnotatedPathDeserializer.java
+++ src/main/java/org/jchien/twitchbrowser/util/AnnotatedPathDeserializer.java
@@ -1,5 +1,6 @@
 package org.jchien.twitchbrowser.util;
 
+import com.google.common.base.Joiner;
 import com.google.gson.*;
 
 import java.lang.annotation.Annotation;
@@ -40,38 +41,20 @@ public class AnnotatedPathDeserializer <E> implements JsonDeserializer<E> {
 
         final JsonObject root = json.getAsJsonObject();
         final Field[] fields = klazz.getDeclaredFields();
-        for (Field f : fields) {
-            final Annotation[] annotations = f.getDeclaredAnnotations();
+        for (Field field : fields) {
+            final Annotation[] annotations = field.getDeclaredAnnotations();
             for (Annotation a : annotations) {
                 if (JsonPath.class == a.annotationType()) {
+                    final JsonPath jsonPath = (JsonPath)a;
+
                     // could avoid repeating work navigating
                     // down same paths for different annotations,
                     // but this is fast enough for now
-                    final JsonPath jsonPath = (JsonPath)a;
-                    final String[] path = jsonPath.path();
-                    JsonObject o = root;
-                    for (int i=0 ; i < path.length - 1; i++) {
-                        o = o.getAsJsonObject(path[i]);
-                    }
-                    final JsonPrimitive p = o.getAsJsonPrimitive(path[path.length-1]);
-
-                    final Class fieldType = f.getType();
-                    // let me access your privates
-                    f.setAccessible(true);
-
-                    // interpret value using appropriate method based on field type
                     try {
-                        if (fieldType == int.class) {
-                            f.set(ret, p.getAsInt());
-                        } else if (fieldType == String.class) {
-                            f.set(ret, p.getAsString());
-                        } else {
-                            throw new RuntimeException("unsupported type: " + fieldType + " for field " + f.getName());
-                        }
-                        //System.out.println("set " + f.getName() + " to " + f.get(ret));
-                    } catch (IllegalAccessException e) {
-                        // blow up for now
-                        throw new RuntimeException("unable to set field " + f.getName(), e);
+                        parseValue(ret, field, jsonPath, root);
+                    } catch (Exception e) {
+                        // blow up for now until logging is handled properly
+                        throw new RuntimeException("unable to parse path " + Joiner.on("/").join(jsonPath.path()), e);
                     }
 
                     // only one annotation of each type is allowed per field,
@@ -81,5 +64,35 @@ public class AnnotatedPathDeserializer <E> implements JsonDeserializer<E> {
             }
         }
         return ret;
+    }
+
+    private void parseValue(final E destObj,
+                            final Field field,
+                            final JsonPath jsonPath,
+                            final JsonObject root) {
+        final String[] path = jsonPath.path();
+        JsonObject o = root;
+        for (int i=0 ; i < path.length - 1; i++) {
+            o = o.getAsJsonObject(path[i]);
+        }
+        final JsonPrimitive p = o.getAsJsonPrimitive(path[path.length-1]);
+
+        final Class fieldType = field.getType();
+        // let me access your privates
+        field.setAccessible(true);
+
+        // interpret value using appropriate method based on field type
+        try {
+            if (fieldType == int.class) {
+                field.set(destObj, p.getAsInt());
+            } else if (fieldType == String.class) {
+                field.set(destObj, p.getAsString());
+            } else {
+                throw new RuntimeException("unsupported type: " + fieldType + " for field " + field.getName());
+            }
+        } catch (IllegalAccessException e) {
+            // blow up for now
+            throw new RuntimeException("unable to set field " + field.getName(), e);
+        }
     }
 }
